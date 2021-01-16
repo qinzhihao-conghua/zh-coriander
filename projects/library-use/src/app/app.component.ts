97592592db0d70279cb20cf2452f350071c8fea1
qinzhihao-conghua <1043419697@qq.com>
2021-01-16 20:36:08 +0800

    radio双向绑定

--- projects/library-use/src/app/app.component.ts
+++ projects/library-use/src/app/app.component.ts
@@ -9,12 +9,21 @@ export class AppComponent {
   title = 'library-use';
 
   checkboxData = [
-    { name: '足球', value: '1', check: true },
-    { name: '篮球', value: '2', check: true },
-    { name: '排球', value: '3', check: false }
+    { name: '足球', value: '1' },
+    { name: '篮球', value: '2' },
+    { name: '排球', value: '3' }
   ];
-  default = ['2'];
+  radioData = [
+    { name: '唱歌', value: '1' },
+    { name: '跳舞', value: '2' },
+    { name: 'rapper', value: '3' }
+  ];
+  checkboxDefault = ['2'];
+  radioDefault = '2';
   modelChange(value): void {
     console.log('点击', value);
+  }
+  radioModelChange(value): void {
+    console.log('radio点击', value);
   }
 }
--- projects/zh-coriander/src/lib/radio/radio.component.ts
+++ projects/zh-coriander/src/lib/radio/radio.component.ts
@@ -1,13 +1,22 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'zh-radio',
   templateUrl: './radio.component.html',
-  styleUrls: ['./radio.component.scss']
+  styleUrls: ['./radio.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => ZhRadioComponent),
+      multi: true
+    }
+  ]
 })
-export class ZhRadioComponent implements OnInit {
+export class ZhRadioComponent implements OnInit, ControlValueAccessor {
 
   constructor() { }
+
   @Input()
   zhDisabled = false;
   @Input()
@@ -15,17 +24,13 @@ export class ZhRadioComponent implements OnInit {
   @Input()
   valueFiled: string;
   @Input()
-  defaultValue: object;
+  canCancel = false;
 
   @Input()
   set data(value: Array<any>) {
-    if (value === undefined || value.length === 0) {
-      this.receiveData = [
-        { name: '是', value: '1', key: 'demo', check: false },
-        { name: '否', value: '0', key: 'demo', check: false }
-      ];
-    } else {
+    if (value !== undefined || value.length > 0) {
       const random = (Math.floor(Math.random() * 1000000)).toString();
+      const temp = [];
       value.forEach((item, index) => {
         const obj = {
           key: `radio-${random}`
@@ -33,42 +38,65 @@ export class ZhRadioComponent implements OnInit {
         if (item instanceof Object) {
           obj['name'] = item[this.textFiled];
           obj['value'] = item[this.valueFiled];
-          if (this.defaultValue && this.defaultValue[this.valueFiled] === item[this.valueFiled]) {
-            obj['check'] = true;
-          } else {
-            obj['check'] = false;
-          }
+          obj['check'] = false;
         } else {
           obj['name'] = item;
           obj['value'] = item;
           obj['check'] = false;
         }
-        this.receiveData.push(obj);
+        temp.push(obj);
       });
+      this.receiveData = [...temp];
     }
   }
   @Output()
-  getRadioValue: EventEmitter<any> = new EventEmitter<any>();
+  modelChange: EventEmitter<any> = new EventEmitter<any>();
+
   receiveData = [];
+  onChange = (newVal: any) => { };
 
   ngOnInit(): void {
-    if (this.receiveData === undefined || this.receiveData.length === 0) {
-      this.receiveData = [
-        { name: '是', value: '1', key: 'demo', check: false },
-        { name: '否', value: '0', key: 'demo', check: false }
-      ];
+  }
+  writeValue(obj: string): void {
+    if (obj) {
+      this.receiveData.forEach(item => {
+        if (item.value === obj) {
+          item.check = true;
+        }
+      });
     }
   }
-  radioCheck(dataItem: object, index: number, event: Event): void {
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    // throw new Error('Method not implemented.');
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    // throw new Error('Method not implemented.');
+  }
+  radioCheck(dataItem: any, index: number, event: Event): void {
     // event.stopPropagation();
     if (this.zhDisabled) {
       return;
     }
     this.receiveData.forEach(item => {
-      item.check = false;
+      if (item.value !== dataItem.value) {
+        item.check = false;
+      }
     });
-    this.receiveData[index].check = true;
+    if (this.canCancel) {
+      dataItem.check = !dataItem.check;
+      if (dataItem.check) {
+        this.onChange(dataItem.value);
+      } else {
+        this.onChange(null);
+      }
+    } else {
+      dataItem.check = true;
+      this.onChange(dataItem.value);
+    }
     // console.log('修改结果', this.receiveData);
-    this.getRadioValue.emit(this.receiveData[index]);
+    this.modelChange.emit(this.receiveData[index]);
   }
 }
